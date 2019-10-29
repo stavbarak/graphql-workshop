9b1ce4080302283cfcfefb6c00671301e8535d1c
Sefi Ninio <sefi@MacBook-Pro-4.local>
2019-10-29 09:28:26 +0200

    limit query depth

--- src/server.js
+++ src/server.js
@@ -1,18 +1,30 @@
 import cors from 'cors';
 import graphqlHttp from 'express-graphql';
 import schema from './schema';
+import depthLimit from 'graphql-depth-limit'
 
 const app = require("express")();
 const PORT = 5000;
 
 app.use(cors());
 
+const DepthLimitRule = depthLimit(
+    4,
+    { ignore: [ 'whatever', 'trusted' ] },
+    depths => console.log(depths)
+  )
+
+
 app.use(graphqlHttp({
   schema,
   // Pretty Print the JSON response
   pretty: true,
   // Enable GraphiQL dev tool
-  graphiql: true
+  graphiql: true,
+  validationRules: [
+    DepthLimitRule,
+  ],
+
 }));
 
 app.listen(PORT, () => {

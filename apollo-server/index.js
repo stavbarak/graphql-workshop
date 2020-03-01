05b36ecec9a0beafe855c9c6094286264a5f039a
Sefi Ninio <sefi.ninio@gmail.com>
2020-03-01 17:32:19 +0200

    apollo-server

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -1,3 +1,17 @@
 import { ApolloServer } from 'apollo-server';
 import resolvers from './resolvers';
 import typeDefs from './resources';
+
+const validationRules = [
+    depthLimit(3),
+];
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    validationRules,
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});

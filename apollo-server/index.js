913913ad16baa50c8b52cc0b633ab28b109e3717
Avishay Maor <avishay@maor.co>
2021-01-17 13:45:09 +0200

    Enable Apollo server subscription

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -20,6 +20,8 @@ const server = new ApolloServer({
     }
 });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
     console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
 });
+
